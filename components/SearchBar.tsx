30502c89bdeb702fd6c62d11309d8baa720be071
hakimxiao <alhakimabusolihin@gmail.com>
2025-11-04 14:40:09 +0700

    - MEMBUAT KOMPONEN BARU YAITU SEARCHBAR YANG BERTUGAS UNTUK MELAKUKKAN PENCARIAN DAN MENTRIGER PERGANTIAN TAB -

---
+++ components/SearchBar.tsx
@@ -0,0 +1,27 @@
+import {View, Text, Image, TextInput} from 'react-native'
+import React from 'react'
+import {icons} from "@/constants/icons";
+
+
+interface SearchBarProps {
+    placeholder: string;
+    onPress?: () => void;
+}
+
+
+const SearchBar = ({ placeholder, onPress }: SearchBarProps) => {
+    return (
+        <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+            <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
+            <TextInput
+                value=""
+                placeholder={placeholder}
+                placeholderTextColor="#ab8bff"
+                className="flex-1 ml-2 text-white"
+                onPress={onPress}
+                onChangeText={() => {}}
+            />
+        </View>
+    )
+}
+export default SearchBar
