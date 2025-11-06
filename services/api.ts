54af266aa9e2435709a07b82d90fa1534a13d183
hakimxiao <alhakimabusolihin@gmail.com>
2025-11-06 11:03:14 +0700

    - MENAMPILKAN HASIL FETCH KE UI INDEX TABS  | MEMBUAT MOVIECARD | MELAKUKKAN INISIALSISASI LAGI DI TAILWIND.CONFIG AGAR COMPONENT DI KENALI MENGGUNAKAN TAILWIND -
    *). Memperbaiki custom Hooks kita yaitu fetchData | Ini sangat disarankan JSMASTERY ya jika kita mempunyai fetch mending buat custom hooks.
    
    *). Membaca semnua data hasil api TMDB ke UI agar dapat di tampilkan
    
    *). Mendeklarasikan lagi folder yang mereka menggunakan TAILWIND | Karena react native berbeda dengan web tailwind disini harus di declare sangat spesifik untuk folder app hanya folder app untuk component hanya component dll.

--- services/api.ts
+++ services/api.ts
@@ -3,14 +3,14 @@ export const  TMDB_CONFIG = {
     API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
     headers: {
         accept: "application/json",
-        Authorization: `Bearer ${process.env.EXPO_API_KEY}`,
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
     }
 }
 
-export const fetchPopularMovies = async({ query }: { query: string }) => {
+export const fetchMovies = async({ query }: { query: string }): Promise<Movie[]> => {
     const endpoint = query
         ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-        : `${TMDB_CONFIG.BASE_URL}/discover/movies?sort_by=popularity.desc`;
+        : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
     const response = await fetch(endpoint, {
         method: "GET",
@@ -24,7 +24,7 @@ export const fetchPopularMovies = async({ query }: { query: string }) => {
 
     const data = await response.json();
 
-    return data.result;
+    return data.results || [];
 }
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-    content: ["./app/**/*.{js,jsx,ts,tsx}"],
+    // content digunakan untuk mendeklarasikan file mana yang bisa mennggunakan tailwind.
+    content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
     presets: [require("nativewind/preset")],
     theme: {
         // NOTE PENTING : ini untuk custom class asli tailwind | Menambahkan hal baru.
